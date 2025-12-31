35a63312ab94d24309f1f3589e0a798b64ec36a6
DeepseekForever <etweteaa@gmail.com>
2025-12-31 09:04:53 +0300

    Update game.js

--- game.js
+++ game.js
@@ -2288,12 +2288,4 @@ window.addEventListener('load', function() {
     
     // Запускаем инициализацию
     setTimeout(init, 100);
-});
-
-// Показываем мобильное управление на телефонах
-if (window.innerWidth <= 768 || 'ontouchstart' in window) {
-    const gameControls = document.querySelector('.game-controls');
-    if (gameControls) {
-        gameControls.style.display = 'flex';
-    }
-}
+})
